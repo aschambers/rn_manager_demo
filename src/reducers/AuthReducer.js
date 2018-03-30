0444d599b24dfbdf221391ff835b6a22fa982c6c
aschambers <aschambers4291@gmail.com>
2018-03-30 08:13:15 -0700

    finish passing value of input on change through redux flow

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -4,7 +4,10 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
 	switch(action.type) {
 		case EMAIL_CHANGED:
-			
+			// get original state, and overrite email property
+			// with new value of email, have to produce new object
+			// or else redux will think nothing changed
+			return {...state, email: action.payload };
 		default:
 			return state;
 	}
