abc575bc79f1ea97185498e05398833b0259bdde
aschambers <aschambers4291@gmail.com>
2018-03-30 09:02:24 -0700

    add login user success type and pass to action and reducer, implement case LOGIN_USER_SUCCESS in auth reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,18 +1,24 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import { 
+	EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS 
+} from '../actions/types';
 // just to let people know what the purpose of the AuthReducer is, not necessary
-const INITIAL_STATE = { email: '', password: '' };
+const INITIAL_STATE = { 
+	email: '', password: '', user: null 
+};
 
 export default (state = INITIAL_STATE, action) => {
 	console.log(action);
-	
+
 	switch(action.type) {
 		case EMAIL_CHANGED:
 			// get original state, and overrite email property
 			// with new value of email, have to produce new object
 			// or else redux will think nothing changed
-			return {...state, email: action.payload };
+			return { ...state, email: action.payload };
 		case PASSWORD_CHANGED:
-			return {...state, password: action.payload };
+			return { ...state, password: action.payload };
+		case LOGIN_USER_SUCCESS:
+			return { ...state, user: action.payload };
 		default:
 			return state;
 	}
