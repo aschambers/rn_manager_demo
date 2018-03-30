f63dfa77e9ea4decdd2cafe859e64d01aec0b301
aschambers <aschambers4291@gmail.com>
2018-03-30 16:09:27 -0700

    finish initialization of forms from state

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 // initial is the route/scene you want to see first
 // add hideNavBar to generate because we already have header for login page
 const RouterComponent = () => {
@@ -21,6 +22,7 @@ const RouterComponent = () => {
 						title="Employees" 
 						initial />
 					<Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+					<Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
 				</Scene>
 			</Scene>
 		</Router>
