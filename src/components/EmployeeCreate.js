035b2b927e2c761d26fa34619274579a12896498
aschambers <aschambers4291@gmail.com>
2018-03-30 11:55:59 -0700

    add picker for the selection of day of the week

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -12,7 +13,7 @@ class EmployeeCreate extends Component {
 						label="Name" 
 						placeholder="Jane" 
 						value={this.props.name} 
-						onChangeText={value => this.props.employeeUpdate({ prop: 'name', value: value })}
+						onChangeText={name => this.props.employeeUpdate({ prop: 'name', value: name })}
 					/>
 				</CardSection>
 				<CardSection>
@@ -20,11 +21,24 @@ class EmployeeCreate extends Component {
 						label="Phone" 
 						placeholder="555-555-5555" 
 						value={this.props.phone} 
-						onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })} 
+						onChangeText={phone => this.props.employeeUpdate({ prop: 'phone', value: phone })} 
 					/>
 				</CardSection>
 				<CardSection>
-					
+					<Picker
+						// expand to fill all available area
+						style={{ flex: 1}}
+						selectedValue={this.props.shift}
+						onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day})}
+					>
+						<Picker.Item label="Monday" values="Monday" />
+						<Picker.Item label="Tuesday" values="Tuesday" />
+						<Picker.Item label="Wednesday" values="Wednesday" />
+						<Picker.Item label="Thursday" values="Thursday" />
+						<Picker.Item label="Friday" values="Friday" />
+						<Picker.Item label="Saturday" values="Saturday" />
+						<Picker.Item label="Sunday" values="Sunday" />
+					</Picker>
 				</CardSection>
 				<CardSection>
 					<Button>
