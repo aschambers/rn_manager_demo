c01954e4dc73ea76894df7c7d5017cd2dcb2fbe0
aschambers <aschambers4291@gmail.com>
2018-03-30 09:12:03 -0700

    add loginUserFail and implement helper functions to reduce ugly code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase';
 import { 
-	EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS 
+	EMAIL_CHANGED, 
+	PASSWORD_CHANGED, 
+	LOGIN_USER_SUCCESS, 
+	LOGIN_USER_FAIL 
 } from './types';
 
 export const emailChanged = (text) => {
@@ -24,27 +27,23 @@ export const loginUser = ({ email, password }) => {
 	// dispatching an action, instead of immediately returning an action
 	return (dispatch) => {
 		firebase.auth().signInWithEmailAndPassword(email, password)
-		.then(user => {
-			dispatch({ 
-				type: LOGIN_USER_SUCCESS, 
-				payload: user 
-			});
-		})
+		// take user and call helper method
+		.then(user => loginUserSuccess(dispatch, user))
 		.catch(() => {
 			firebase.auth().createUserWithEmailAndPassword(email, password)
-			.then(user => {
-				dispatch({ 
-					type: LOGIN_USER_SUCCESS, 
-					payload: user 
-				});
-			});
+			.then(user => loginUserSuccess(dispatch, user))
+			.catch(() => loginUserFail(dispatch));
 		});
 	};
 };
 
-// const loginUserSuccess = (dispatch, user) => {
-// 	dispatch({
-// 		type: LOGIN_USER_SUCCESS,
-// 		payload: user
-// 	})
-// }
+const loginUserFail = (dispatch) => {
+	dispatch({ type: LOGIN_USER_FAIL })
+}
+
+const loginUserSuccess = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user
+	})
+}
