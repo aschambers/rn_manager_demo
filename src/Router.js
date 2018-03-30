8241ebe566982533e87b4b28a7ec39a65149c194
aschambers <aschambers4291@gmail.com>
2018-03-30 11:06:30 -0700

    add right nav button to navbar in main nav when logged in

--- src/Router.js
+++ src/Router.js
@@ -12,7 +12,12 @@ const RouterComponent = () => {
 					<Scene key="login" component={LoginForm} title="Please Login" initial />
 				</Scene>
 				<Scene key="main">
-					<Scene key="employeeList" component={EmployeeList} title="Employees" />
+					<Scene 
+						rightTitle="Add"
+						onRight={() => { console.log('right button in main nav') }}
+						key="employeeList" 
+						component={EmployeeList} 
+						title="Employees" />
 				</Scene>
 			</Scene>
 		</Router>
