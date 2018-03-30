bb33245a659c00fc96df51b46096688ccb32f757
aschambers <aschambers4291@gmail.com>
2018-03-30 13:27:46 -0700

    add data source for getting employees in employee list component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
 	// as soon as it's about to be rendered, fetch list of employees
+	// on first load, no employees, but then will attempt to get list of
+	// employees
 	componentWillMount() {
 		this.props.employeesFetch();
+			
+		this.createDataSource(this.props);
 	}
+	// recreate data source, this time we will have access to employees
+	componentWillReceiveProps(nextProps) {
+		// nextProps are the next set of props that this component
+		// will be rendered with. this.props, is still the old set of props
+		this.createDataSource(nextProps);
+	}
+	// be called with a props object, always be attempting to create a data source
+	createDataSource({ employees }) {
+		const lvds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		});
+		// will be empty as the request won't quite be complete
+		this.dataSource = lvds.cloneWithRows(employees);
+	}
+
 	render() {
 		return (
 			<View>
