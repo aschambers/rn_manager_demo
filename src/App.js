dce3d6870c44fd4a82b787d6ef288d9c6406a148
aschambers <aschambers4291@gmail.com>
2018-03-30 06:31:13 -0700

    boilerplate setup with firebase

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+	componentWillMount() {
+		const config = {
+		    apiKey: 'AIzaSyBESCKN4t9a458KQt1sIwhIsKK5xKu_bIY',
+		    authDomain: 'vuejs-http-7d991.firebaseapp.com',
+		    databaseURL: 'https://vuejs-http-7d991.firebaseio.com',
+		    projectId: 'vuejs-http-7d991',
+		    storageBucket: 'vuejs-http-7d991.appspot.com',
+		    messagingSenderId: '9584545554'
+		};
+		firebase.initializeApp(config);
+	}
+	render() {
+		return (
+			<Provider store={createStore(reducers)}>
+				<View>
+					<Text>
+						I'm the main App
+					</Text>
+				</View>
+			</Provider>
+		);
+	}
+}
+
+export default App;
