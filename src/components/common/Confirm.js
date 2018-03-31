bbfa95a38ea6cba6e976dc0c5b5478bb724cab4d
aschambers <aschambers4291@gmail.com>
2018-03-30 17:15:48 -0700

    add modal to confirm yes or no in common

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -2,9 +2,30 @@ import React from 'react';
 import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './CardSection';
-
-const Confirm = () => {
-
+// need the children prop to use { children } in Text section
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+	// will show up as props.children {children}
+	// onAccept, is just a reference, same thing with onDecline, call when needed
+	return (
+		<Modal
+			visible={visible}
+			transparent
+			animationType="slide"
+			onRequestClose={() => {}}
+		>
+			<View>
+				<CardSection>
+					<Text>
+						{children}
+					</Text>
+				</CardSection>
+				<CardSection>
+					<Button whenClicked={onAccept}>Yes</Button>
+					<Button whenClicked={onDecline}>No</Button>
+				</CardSection>
+			</View>
+		</Modal>
+	);
 };
 
 export { Confirm };
