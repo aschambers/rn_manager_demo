6a6473a28d7c9c63bd7919a7ad1150254f080e3c
aschambers <aschambers4291@gmail.com>
2018-03-30 17:10:47 -0700

    add modal for confirming

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './CardSection';
+
+const Confirm = () => {
+
+};
+
+export { Confirm };
